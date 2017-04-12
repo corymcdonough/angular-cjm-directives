317cb877afea07764ccc6448c4fb9be4292af5bc
Cory McDonough <corymcdonough@users.noreply.github.com>
2017-04-12 10:14:15 -0500

    Remove dead code and log statement, use angular.element.parent()

--- src/bounding/bounding.js
+++ src/bounding/bounding.js
@@ -4,60 +4,46 @@ angular.module('cjm.directives.bounding', [])
     return {
       getBoundingContainer: function(element, boundingElement) {
           var container = false;
+          var boundToAncestor = false;
+          var absoluteOffsetX = 0;
+          var absoluteOffsetY = 0;
 
-          // if(boundingElement[0] === element[0].parentElement) {
-          //   container = {
-          //     left: 0,
-          //     top: 0,
-          //     width: boundingElement[0].offsetWidth,
-          //     height: boundingElement[0].offsetHeight,
-          //     right: boundingElement[0].offsetWidth,
-          //     bottom: boundingElement[0].offsetHeight
-          //   };
-          // } else {
-            var boundToAncestor = false;
-            var absoluteAncestor = false;
-            var absoluteOffsetX = 0;
-            var absoluteOffsetY = 0;
-            if(boundingElement) {
-              var ancestorElement = element;
-              while (ancestorElement[0].parentElement && !boundToAncestor) {
-                ancestorElement = angular.element(ancestorElement[0].parentElement);
-                if(ancestorElement[0] === boundingElement[0]) {
-                  console.log('bound to ancestor');
-                  boundToAncestor = true;
-                }
-                if($window.getComputedStyle(ancestorElement[0], null).getPropertyValue('position') === 'absolute') {
-                  absoluteAncestor = true;
-                  absoluteOffsetX += ancestorElement[0].offsetLeft;
-                  absoluteOffsetY += ancestorElement[0].offsetTop;
-                }
+          if(boundingElement) {
+            var ancestorElement = element;
+            while (ancestorElement[0].parentElement && !boundToAncestor) {
+              ancestorElement = ancestorElement.parent();
+              if(ancestorElement[0] === boundingElement[0]) {
+                boundToAncestor = true;
               }
-
-              if(boundToAncestor) {
-                container = {
-                  left: boundingElement[0].offsetLeft - absoluteOffsetX,
-                  top: boundingElement[0].offsetTop - absoluteOffsetY,
-                  width: boundingElement[0].offsetWidth,
-                  height: boundingElement[0].offsetHeight,
-                  right: 0,
-                  bottom: 0
-                };
-              } else {
-                var rectBounding = boundingElement[0].getBoundingClientRect();
-                container = {
-                  left: rectBounding.left - absoluteOffsetX + window.scrollX,
-                  top: rectBounding.top - absoluteOffsetY + window.scrollY,
-                  width: boundingElement[0].offsetWidth,
-                  height: boundingElement[0].offsetHeight,
-                  right: 0,
-                  bottom: 0
-                };
+              if($window.getComputedStyle(ancestorElement[0], null).getPropertyValue('position') === 'absolute') {
+                absoluteOffsetX += ancestorElement[0].offsetLeft;
+                absoluteOffsetY += ancestorElement[0].offsetTop;
               }
-              container.right = container.left + container.width;
-              container.bottom = container.top + container.height;
             }
-          // }
+
+            if(boundToAncestor) {
+              container = {
+                left: boundingElement[0].offsetLeft - absoluteOffsetX,
+                top: boundingElement[0].offsetTop - absoluteOffsetY,
+                width: boundingElement[0].offsetWidth,
+                height: boundingElement[0].offsetHeight,
+                right: 0,
+                bottom: 0
+              };
+            } else {
+              var rectBounding = boundingElement[0].getBoundingClientRect();
+              container = {
+                left: rectBounding.left - absoluteOffsetX + window.scrollX,
+                top: rectBounding.top - absoluteOffsetY + window.scrollY,
+                width: boundingElement[0].offsetWidth,
+                height: boundingElement[0].offsetHeight,
+                right: 0,
+                bottom: 0
+              };
+            }
+            container.right = container.left + container.width;
+            container.bottom = container.top + container.height;
+          }
 
           return container;
         }
